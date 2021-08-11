c4a02f6d58108f84a7b9e7e3765c39b3b61a4522
lucian <37814580+lucianmot@users.noreply.github.com>
2021-08-11 09:59:55 +0100

    Update calc-03.py

--- calc-03.py
+++ calc-03.py
@@ -1,1 +1,33 @@
-# test4
+
+PLUS, MINUS, INTEGER, EOF = "PLUS", "MINUS", "INTEGER", "EOF";
+
+class Tokenizer (object) :
+# class Token (object) :
+    def __init__ (self, type, value) :
+        self.type = type;
+        self.value = value;
+
+    def create (self) : 
+        return 'Token({type}, {value})'.format(
+            type = self.type,
+            value = repr(self.value)
+        );
+
+    def __repr__ (self) :
+        return self.create();
+
+# create class (lexer) to recognize blocks divided by space, tokenize and return to main (where they will be printed)
+
+def main ():
+    while True:
+        try:
+            text = input ("Please enter: ");
+        except EOFError:
+            break;
+        if not text:
+            continue;
+
+        
+    
+if __name__ == '__main__':
+    main()
